fb1815edc98d8f3de419eea050bbe1efe53fe1c7
saroj kumar sah <sarojahah152@gmail.com>
2024-01-08 10:26:52 +0545

    blankchart for graphs

---
+++ src/main/java/blankchart/BlankPlotChatRender.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package blankchart;
+
+
+import java.awt.Graphics2D;
+import java.awt.event.MouseEvent;
+import java.awt.geom.Rectangle2D;
+
+public abstract class BlankPlotChatRender {
+
+    public abstract String getLabelText(int index);
+
+    public abstract void renderGraphics(BlankPlotChart chart, Graphics2D g2, Rectangle2D rectangle);
+
+    public abstract void mouseMove(Rectangle2D rectangle, MouseEvent mouse);
+}
