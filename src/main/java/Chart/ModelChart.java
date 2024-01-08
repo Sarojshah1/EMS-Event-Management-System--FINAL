48262b513a4e81e99304b3223bac9a28e7e4cb65
saroj kumar sah <sarojahah152@gmail.com>
2024-01-08 10:30:51 +0545

    Chart

---
+++ src/main/java/Chart/ModelChart.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Chart;
+
+
+public class ModelChart {
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public double[] getValues() {
+        return values;
+    }
+
+    public void setValues(double[] values) {
+        this.values = values;
+    }
+
+    public ModelChart(String label, double[] values) {
+        this.label = label;
+        this.values = values;
+    }
+
+    public ModelChart() {
+    }
+
+    private String label;
+    private double values[];
+
+    public double getMaxValues() {
+        double max = 0;
+        for (double v : values) {
+            if (v > max) {
+                max = v;
+            }
+        }
+        return max;
+    }
+}
---
+++ src/main/java/Chart/ModelChart_1.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chart;
+
+
+public class ModelChart_1 {
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public double[] getValues() {
+        return values;
+    }
+
+    public void setValues(double[] values) {
+        this.values = values;
+    }
+
+    public ModelChart_1(String label, double[] values) {
+        this.label = label;
+        this.values = values;
+    }
+
+    public ModelChart_1() {
+    }
+
+    private String label;
+    private double values[];
+
+    public double getMaxValues() {
+        double max = 0;
+        for (double v : values) {
+            if (v > max) {
+                max = v;
+            }
+        }
+        return max;
+    }
+}
